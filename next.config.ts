294a672ae46b299e5cfc364527fe6c62131346fe
Grace Xu <71201761+gxu0904@users.noreply.github.com>
2025-10-29 22:27:54 -0700

    Configure GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  output: 'export',
+  basePath: '/monster',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
